efc44c8685a3e4645cab3d868a630b21da2843a8
Emmanuel Ejoor <140710593+huios2023@users.noreply.github.com>
2024-11-16 12:15:58 +0100

    strategic partnership contract

--- strategic_partnership_contract/models/models.py
+++ strategic_partnership_contract/models/models.py
@@ -2,33 +2,29 @@ from odoo import models, fields, api
 from datetime import date, datetime
 
 class support_contract(models.Model):
-    _name = 'support.contract'
-    _description = 'Support Contract'
+    _name = 'strategic.partnership.contract'
+    _description = 'Strategic Partnership Contract'
     _inherit = 'contract.manager'
 
 
-    contract_type = fields.Selection([('o','On Premise'),('c','Call Out')])
-    callout_type = fields.Selection([('p','Pay as You Go'),('c','Contractual')])
-    # invoice_filename = fields.Char(string="File Name")
-    # invoice_filename
-    invoice = fields.Binary(string='Invoice', attachment=True)
+
 
     @api.depends()
     def _compute_contract_ref(self):
         for record in self:
             contract_number = record.id
             if contract_number < 10:
-                record.contract_id = 'LBA/SUPPORT/CONTRACT/00{}'.format(contract_number)
+                record.contract_id = 'LBA/SP/CONTRACT/00{}'.format(contract_number)
             elif 10 == contract_number < 100:
-                record.contract_id = 'LBA/SUPPORT/CONTRACT/0{}'.format(contract_number)
+                record.contract_id = 'LBA/SP/CONTRACT/0{}'.format(contract_number)
             else:
-                record.contract_id = 'LBA/SUPPORT/CONTRACT/{}'.format(contract_number)
+                record.contract_id = 'LBA/SP/CONTRACT/{}'.format(contract_number)
     
 
     @api.model
     def send_notification_email(self):
-        template = self.env.ref('support_contract.email_template_support_contract')
-        contracts = self.env['support.contract'].search([('status', '=', 'running')])
+        template = self.env.ref('strategic_partnership_contract.email_template_strategic_partnership_contract')
+        contracts = self.env['strategic.partnership.contract'].search([('status', '=', 'running')])
         for record in contracts:
             email = record.client.email
             start_date = record.start_date 
