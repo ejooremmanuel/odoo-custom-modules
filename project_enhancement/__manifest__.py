dc7c62d2369199be88f20116b7e36047c701a1a6
Emmanuel Ejoor <140710593+huios2023@users.noreply.github.com>
2024-11-26 14:24:07 +0100

    update manifest

--- project_enhancement/__manifest__.py
+++ project_enhancement/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "project_enhancement",
+    'name': "Project Enhancement",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -16,11 +16,11 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Services/Project Enhancement',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','project.project'],
 
     # always loaded
     'data': [
